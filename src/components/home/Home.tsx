88e1aa28b9bb58fcf24495f8bf67f59adbe6e738
RaykoRadev <christmas898@gmail.com>
2026-03-10 15:06:33 +0100

    fixing the left text in the middle

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <>
             {/* title */}
             <div className="flex flex-col items-center align-middle p-13">
-                <h1 className="mt-20 font-syne font-bold text-6xl text-center text-space tracking-wide">
+                <h1 className="font-syne font-bold text-6xl text-center text-space tracking-wide">
                     Adaptive Logo Design for Your Brand
                 </h1>
                 <Button className="mt-20 bg-orange-400 rounded-full px-7 py-4 font-rubik font-bold text-sm text-slate-100 tracking-[1.63px] self-center">
@@ -15,7 +15,7 @@ export default function Home() {
 
             {/* me */}
             <div className="flex justify-between p-13 gap-10">
-                <div className="w-3/5">
+                <div className="w-3/5 self-center">
                     <h2 className="font-syne font-bold text-3xl text-space tracking-wide">
                         Let's get know about me closer
                     </h2>
@@ -30,9 +30,11 @@ export default function Home() {
                         Discover more about me
                     </Button>
                 </div>
-                <div className="w-113.5 h-126.5 overflow-hidden rounded-2xl">
+                <div className="relative w-113.5 h-126.5  rounded-2xl">
+                    {/* <p className="border border-orange-400 w-27.5 h-8.5 z-50 rounded-2xl absolute -top-4 right-0"></p>
+                    <p className="border border-orange-400 w-12.5 h-44.5 z-50 rounded-3xl absolute bottom-8 -left-6"></p> */}
                     <img
-                        className="w-full h-full object-cover object-right opacity-70"
+                        className="w-full h-full object-cover object-right opacity-70 rounded-2xl"
                         src="me.jpg"
                         alt="me"
                     />
