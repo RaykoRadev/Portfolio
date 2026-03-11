bd2e306aa438ea331e775bae58dd53910b189877
RaykoRadev <christmas898@gmail.com>
2026-03-11 16:06:51 +0100

    file containing all projects data

---
+++ src/assets/projectsDB.ts
@@ -0,0 +1,33 @@
+export const projects = [
+    {
+        _id: "8",
+        img: "https://i.ibb.co/fVcy8xqb/smart-and-happy.png",
+        title: "Smart and Happy",
+        technology: "React, Tailwind, TanStack Query, Sonner",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
+    },
+    {
+        _id: "7",
+        img: "https://i.ibb.co/CpC49Xd3/pet-shelter.png",
+        title: "Pet shelter refactored",
+        technology: "React, Tailwind, TypeScript, TanStack Query, Zod, Sonner",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
+    },
+    {
+        _id: "6",
+        img: "https://i.ibb.co/CpC49Xd3/pet-shelter.png",
+        title: "Pet shelter",
+        technology: "React, Tailwind",
+        description: "",
+    },
+
+    {
+        _id: "1",
+        img: "https://i.ibb.co/rKP3KKwY/handmade-gift-cards.png",
+        title: "Handmade gift cards",
+        technology: "Angular, TypeScript",
+        description: "",
+    },
+];
