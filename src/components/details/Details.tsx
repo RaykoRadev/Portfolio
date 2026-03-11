1f810e0fb8d56136efd0a530835b3c4a9fe92b7f
RaykoRadev <christmas898@gmail.com>
2026-03-11 16:41:24 +0100

    adding exp details in Details page

--- src/components/details/Details.tsx
+++ src/components/details/Details.tsx
@@ -1,3 +1,30 @@
+import PageTitle from "../page-title/PageTitle";
+
 export default function Details() {
-    return <>Details works</>;
+    return (
+        <>
+            <PageTitle name="Details" info="Details About The Project" />
+            <div>
+                <img
+                    className="rounded-2xl"
+                    src="https://i.ibb.co/fVcy8xqb/smart-and-happy.png"
+                    alt=""
+                />
+                <h5 className="mt-5 font-syne text-2xl">Smart and Happy</h5>
+
+                <p className="mt-5">
+                    technology: "React, Tailwind, TypeScript, TanStack Query,
+                    Zod, Sonner"
+                </p>
+                <p className="mt-5">Links:"</p>
+                <p className="mt-5">server: git:..... deploy: ......"</p>
+                <p className="mt-5">app: git:..... deploy: ......"</p>
+                <p className="mt-5">
+                    Desctription: Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Enim minima autem accusantium officiis sed
+                    facilis tempora perspiciatis veniam quam animi!
+                </p>
+            </div>
+        </>
+    );
 }
