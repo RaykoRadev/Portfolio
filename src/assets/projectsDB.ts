6f9763ffe6a49e4cad0ac281b22144bb41eada0e
RaykoRadev <christmas898@gmail.com>
2026-03-12 11:50:10 +0100

    update the project file

--- src/assets/projectsDB.ts
+++ src/assets/projectsDB.ts
@@ -4,6 +4,14 @@ export const projects = [
         img: "https://i.ibb.co/fVcy8xqb/smart-and-happy.png",
         title: "Smart and Happy",
         technology: "React, Tailwind, TanStack Query, Sonner",
+        app: {
+            git: "https://github.com/RaykoRadev/school-puzzle-and-tasks",
+            deploy: "https://smart-and-happy.web.app/",
+        },
+        server: {
+            git: "https://github.com/RaykoRadev/server-school-project",
+            deploy: "https://dashboard.render.com/project/prj-d65ghehr0fns738aqgc0",
+        },
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
     },
@@ -12,6 +20,14 @@ export const projects = [
         img: "https://i.ibb.co/CpC49Xd3/pet-shelter.png",
         title: "Pet shelter refactored",
         technology: "React, Tailwind, TypeScript, TanStack Query, Zod, Sonner",
+        app: {
+            git: "https://github.com/RaykoRadev/Pet-Shelter-React-Course-project-TS",
+            deploy: "",
+        },
+        server: {
+            git: "https://github.com/RaykoRadev/back-end-server---nodeJS-express",
+            deploy: "https://dashboard.render.com/web/srv-d4m1libe5dus73826s20",
+        },
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
     },
@@ -20,6 +36,14 @@ export const projects = [
         img: "https://i.ibb.co/CpC49Xd3/pet-shelter.png",
         title: "Pet shelter",
         technology: "React, Tailwind",
+        app: {
+            git: "https://github.com/RaykoRadev/Pet-Shelter-React-Course-project",
+            deploy: "https://pet-shelter-react-course-project.vercel.app/",
+        },
+        server: {
+            git: "https://github.com/RaykoRadev/back-end-server---nodeJS-express",
+            deploy: "https://dashboard.render.com/web/srv-d4m1libe5dus73826s20",
+        },
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
     },
@@ -29,7 +53,20 @@ export const projects = [
         img: "https://i.ibb.co/rKP3KKwY/handmade-gift-cards.png",
         title: "Handmade gift cards",
         technology: "Angular, TypeScript",
+        app: {
+            git: "https://github.com/RaykoRadev/Angular-Project",
+            deploy: "https://handmade-cards-by-fresia.vercel.app/home",
+        },
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim minima autem accusantium officiis sed facilis tempora perspiciatis veniam quam animi!",
     },
+    // {
+    //     _id: "",
+    //     img: "",
+    //     title: "",
+    //     technology: "",
+    //     app: { git: "", deploy: "" },
+    //     server: { git: "", deploy: "" },
+    //     description: "",
+    // },
 ];
